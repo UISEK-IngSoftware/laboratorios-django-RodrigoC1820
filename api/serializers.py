5c266e4e876e3398b26f0e3da5c4b62ae9e06d4e
RodrigoC <rodrigo.castillo@uisek.edu.ec>
2026-06-08 20:13:54 -0500

    Finalizado laboratorio 7

--- api/serializers.py
+++ api/serializers.py
@@ -1,24 +1,32 @@
 from rest_framework import serializers
-from pokedex.models import Pokemon
+from pokedex.models import Pokemon, Trainer
 from django.core.files.base import ContentFile
 import base64
 
-class PokemonSerializer(serializers.ModelSerializer):
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str):
+            if data == '' or data is None:
+                return None
+            if ';base64,' in data:
+                try:
+                    format, imgstr = data.split(';base64,')
+                    ext = format.split('/')[-1]
+                    return ContentFile(base64.b64decode(imgstr), name=f'image.{ext}')
+                except Exception:
+                    raise serializers.ValidationError('La imagen no se encuentra con base64 válida.')
+        return super().to_internal_value(data)
 
-    picture = serializers.CharField(required=False, allow_blank=True)
+class TrainerSerializer(serializers.ModelSerializer):
+    photo = Base64ImageField(required=False, allow_null=True, use_url=True)
+
+    class Meta:
+        model = Trainer
+        fields = '__all__'
+
+class PokemonSerializer(serializers.ModelSerializer):
+    picture = Base64ImageField(required=False, allow_null=True, use_url=True)
 
     class Meta:
         model = Pokemon
         fields = '__all__'
-    def validate_picture(self, value):
-        if value:
-            try:
-                format, imgstr = value.split(';base64,')
-                ext = format.split('/')[-1]
-                return ContentFile(
-                    base64.b64decode(imgstr),
-                    name=f'pokemon.{ext}'
-                )
-            except Exception:
-                raise serializers.ValidationError("La imagen no se encuentra con base64 válida.")
-        return value
--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
-from pokedex.models import Pokemon
-from .serializers import PokemonSerializer  
+from pokedex.models import Pokemon, Trainer
+from .serializers import PokemonSerializer, TrainerSerializer
 
 class PokemonViewSet(viewsets.ModelViewSet):
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
+
+class TrainerViewSet(viewsets.ModelViewSet):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
